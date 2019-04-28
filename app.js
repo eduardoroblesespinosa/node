e4c57f65b1a00d2afb56f719d3688172e4717752
eduardoroblesespinosa <47559667+eduardoroblesespinosa@users.noreply.github.com>
2019-04-28 18:22:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const http = require('http');
-const port=process.env.PORT || 3000
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
-});
-server.listen(port,() => {
-console.log(`Server running at port `+port);
-});
+
+//desde aqui
+const server = require('server');
+const { get, socket } = server.router;
+const { render } = server.reply;
+
+// Update everyone with the current user count
+const updateCounter = ctx => {
+  ctx.io.emit('count', Object.keys(ctx.io.sockets.sockets).length);
+};
+
+// Send the new message to everyone
+const sendMessage = ctx => {
+  ctx.io.emit('message', ctx.data);
+};
+
+server([
+  get('/', ctx => render('index.html')),
+  socket('connect', updateCounter),
+  socket('disconnect', updateCounter),
+  socket('message', sendMessage)
+]);
 
