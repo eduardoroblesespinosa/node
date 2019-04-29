71ace3d78322f16e43db4412c43d4c9ff8eb4df7
eduardoroblesespinosa <47559667+eduardoroblesespinosa@users.noreply.github.com>
2019-04-29 02:16:52 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const port=process.env.PORT;
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
+//res.end('<h1>Hello World</h1>');
 });
 server.listen(port,() => {
 console.log(`Server running at port `+port);
@@ -14,5 +14,8 @@ const renede = server.reply;
 const updateCounter = ctx => {
   ctx.io.emit('count', Object.keys(ctx.io.sockets.sockets).length);
 };
+server([
+  // For the initial load render the index.html
+  get('/', ctx => render('index.html')),
 
 
