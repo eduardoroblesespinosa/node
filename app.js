4a3c94c5cb9e83cd7ac9f231137652319fb3860d
eduardoroblesespinosa <47559667+eduardoroblesespinosa@users.noreply.github.com>
2019-04-29 02:07:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-const server = require('server');
+const http = require('http');
+//const port=process.env.PORT || 3000
+const server = http.createServer((req, res) => {
+res.statusCode = 200;
+res.setHeader('Content-Type', 'text/html');
+res.end('<h1>Hello World</h1>');
+});
+server.listen(port,() => {
+console.log(`Server running at port `+port);
+});
 const get = server.router;
 const socket = server.router;
 const renede = server.reply;
