45cd5bbde4844fb645ef53875c132dcf115a585b
eduardoroblesespinosa <47559667+eduardoroblesespinosa@users.noreply.github.com>
2019-04-29 21:01:19 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const http = require('http');
+var dt = require('./modulo1');
 const port=process.env.PORT;
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
+res.write("The date and time are currently: " + dt.myDateTime());
 res.end();
 });
 server.listen(port,() => {
