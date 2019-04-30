cd8df5d43b0af2f06695d4f14fbc035d03ebd5cf
eduardoroblesespinosa <47559667+eduardoroblesespinosa@users.noreply.github.com>
2019-04-30 10:54:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const http = require('http');
+var http = require('http');
 var dt = require('./modulo1');
+var fs = require('fs');
 const port=process.env.PORT;
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.write("The date and time are currently: " + dt.myDateTime());
-res.end();
-});
-server.listen(port,() => {
-console.log(`Server running at port `+port);
+http.createServer(function (req, res) {
+  //Open a file on the server and return its content:
+  fs.readFile('./index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
 });
 
 
