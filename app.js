52b69daab4fecd8fd92831463d78a32262d3ac41
eduardoroblesespinosa <47559667+eduardoroblesespinosa@users.noreply.github.com>
2019-04-30 11:01:01 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 var dt = require('./modulo1');
 var fs = require('fs');
-const port=process.env.PORT;
+//const port=process.env.PORT;
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
