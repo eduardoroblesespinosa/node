4a04f64fdd6785378703149e73bc13fa5553703b
eduardoroblesespinosa <47559667+eduardoroblesespinosa@users.noreply.github.com>
2019-05-01 09:49:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,4 @@
-const http = require('http');
-var express = require('express');
-var app = express();
-app.set('view engine','ejs');
-app.use('/public',express.static('public'));
-//var dt = require('./style.css');
-//var dt = require('./modulo1');
-var fs = require('fs');
-const port=process.env.PORT;
-const server = http.createServer((req, res) => {
-fs.readFile('./index.html', function(err, data) {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.write(data);
-res.end();
-});
-  });
-  
-server.listen(port,() => {
-console.log(`Server running at port `+port);
-});
-
-
-
+const express = require('express')
+const app = express()
+app.use(express.static('public'))
+app.listen(process.env.PORT || 8080, ()=> console.log('All ok!'))
